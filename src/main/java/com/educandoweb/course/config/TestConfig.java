6a6309a4aad42b6c64f2970c11ead21488184ea7
henricobela <henrico.nardelli@hotmail.com>
2020-10-14 09:45:16 -0300

    ManyToMany

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -34,7 +34,7 @@ public class TestConfig implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-				
+
 		User u1 = new User("Maria Brown", "maria@gmail.com", "988888888", Encrypt.encrypt("15151515"));
 		User u2 = new User("Alex Green", "alex@gmail.com", "977777777", Encrypt.encrypt("1515151556556"));
 
@@ -45,7 +45,7 @@ public class TestConfig implements CommandLineRunner {
 		Category cat1 = new Category("Electronics");
 		Category cat2 = new Category("Books");
 		Category cat3 = new Category("Computers");
-		
+
 		Product p1 = new Product("The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
 		Product p2 = new Product("Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
 		Product p3 = new Product("Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
@@ -56,6 +56,16 @@ public class TestConfig implements CommandLineRunner {
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
 	}
 
 }
